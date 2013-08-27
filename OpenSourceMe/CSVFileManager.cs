6c8f31bb3a9757ca97e2056c85eba087f5029ff1
Romain Piquois <rpiquois@klab.com>
2013-08-27 14:48:19 +0900

    More secure support and easier detection of issues. (Do not split when using comments.

--- OpenSourceMe/CSVFileManager.cs
+++ OpenSourceMe/CSVFileManager.cs
@@ -22,10 +22,29 @@ namespace OpenSourceMe
             m_header = lines[0].Split(s_separators);
 
             for(int i = 1; i < lines.Length; ++i) {
-                m_values[i - 1] = new String[m_header.Length];
-                String[] tmp = lines[i].Split(s_separators);
-                for(int j = 0; j < tmp.Length; ++j) {
-                    m_values[i - 1][j] = tmp[j];
+                String[] tmp = null;
+                try
+                {
+                    m_values[i - 1] = new String[m_header.Length];
+                    tmp = lines[i].Split(s_separators);
+                    if (tmp[0].ToString().StartsWith("//"))
+                    {
+                        for (int j = 0; j < m_header.Length; ++j)
+                        {
+                            m_values[i - 1][j] = null;
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 0; j < tmp.Length; ++j)
+                        {
+                            m_values[i - 1][j] = tmp[j];
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Invalid CSV file format at line " + i);
                 }
             }
         }
